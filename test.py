1efd1e8aed92698e8c9da414918543a1278d9473
Hitesh lalwani <hlalwani28022001@gmail.com>
2025-06-28 00:34:48 +0530

    test pushed

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print("hello one")
-print("three")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+s= Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.maximize_window()
+page = driver.get("https://google.com")
+print(page)
